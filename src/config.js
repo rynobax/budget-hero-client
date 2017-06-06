b747741a60946ea4575f0442778dac08aeb7ac57
Ryan Baxley <rbaxley@sourcelink.com>
2017-06-06 15:16:27 -0400

    Config now uses aws API in production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
-/* This config file is modified during the travis build process to
-   populate with production values */
-
+let API_BASE = "";
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+    API_BASE = "http://localhost:9000/api/";
+}else {
+    API_BASE = "https://wp7ut8hoe3.execute-api.us-east-1.amazonaws.com/prod/api/";
+}
 module.exports = {
-    API_BASE: "http://localhost:9000/api/"
+    API_BASE: API_BASE
 };
